01a32acc9060ff01d06b4746b98deab84cbaf872
Mabaria <magnomrikard@gmail.com>
2017-02-16 12:21:41 +0100

    Vidare progress på OBJ-loader

--- DV1542-Projekt/OBJLoader.h
+++ DV1542-Projekt/OBJLoader.h
@@ -4,11 +4,18 @@
 #include <d3d11.h>
 #include <d3dcompiler.h>
 
+struct Vertex
+{
+	float x, y, z;
+	float r, g, b;
+	float u, v;
+};
 
 using namespace DirectX;
 
 
-bool loadOBJ(const char* filePath, std::vector <XMFLOAT3> &vertices, std::vector <XMFLOAT2> &texCoords, std::vector <XMFLOAT3> &normals) {
+bool loadOBJ(const char* filePath, std::vector <Vertex> &vertices) {
+	std::vector <unsigned int> vertexIndices, texCoordIndices, normalIndices;
 	std::vector <XMFLOAT3> tempVerts;
 	std::vector <XMFLOAT2> tempTexCoords;
 	std::vector <XMFLOAT3> tempNormals;
@@ -30,6 +37,38 @@ bool loadOBJ(const char* filePath, std::vector <XMFLOAT3> &vertices, std::vector
 				XMFLOAT3 vertex;
 				fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
 				tempVerts.push_back(vertex);
+			}
+			else if (strcmp(lineHeader, "vt") == 0) { // Current line to read is texcoord info
+				XMFLOAT2 texCoord;
+				fscanf(file, "%f %f\n", &texCoord.x, &texCoord.y);
+				tempTexCoords.push_back(texCoord);
+			}
+			else if (strcmp(lineHeader, "vn") == 0) { // Current line to read is normal info
+				XMFLOAT3 normal;
+				fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+				tempNormals.push_back(normal);
+			}
+			else if (strcmp(lineHeader, "f") == 0) { // Current line to read is face info
+				unsigned int vertexIndex[3], texCoordIndex[3], normalIndex[3];
+				int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &texCoordIndex[0], &normalIndex[0], 
+					&vertexIndex[1], &texCoordIndex[1], &normalIndex[1], &vertexIndex[2], &texCoordIndex[2], &normalIndex[2]);
+				if (matches != 9) {
+					MessageBox(NULL, L"Face data in OBJ file does not match format of the OBJ reader!", NULL, MB_ICONEXCLAMATION);
+					return false;
+				}
+				else {
+					vertexIndices.push_back(vertexIndex[0]);
+					vertexIndices.push_back(vertexIndex[1]);
+					vertexIndices.push_back(vertexIndex[2]);
+					texCoordIndices.push_back(texCoordIndex[0]);
+					texCoordIndices.push_back(texCoordIndex[1]);
+					texCoordIndices.push_back(texCoordIndex[2]);
+					normalIndices.push_back(normalIndex[0]);
+					normalIndices.push_back(normalIndex[1]);
+					normalIndices.push_back(normalIndex[2]);
+
+				}
+
 			}
 		}
 
