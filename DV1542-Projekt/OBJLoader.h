0fb4ff33a7af7a5fe46ae16b144be5dab4e92fb1
Mabaria <magnomrikard@gmail.com>
2017-02-16 13:35:17 +0100

    fscanf_SSSSSSSSSS

--- DV1542-Projekt/OBJLoader.h
+++ DV1542-Projekt/OBJLoader.h
@@ -15,12 +15,20 @@ using namespace DirectX;
 
 
 bool loadOBJ(const char* filePath, std::vector <Vertex> &vertices) {
+	// This function reads obj files of format
+	// v 1 1 1
+	// vt 1 1 
+	// vn 1 1 1
+	// f 1/1/1 1/1/1 1/1/1
+
+
 	std::vector <unsigned int> vertexIndices, texCoordIndices, normalIndices;
 	std::vector <XMFLOAT3> tempVerts;
 	std::vector <XMFLOAT2> tempTexCoords;
 	std::vector <XMFLOAT3> tempNormals;
 
-	FILE * file = fopen(filePath, "r"); // Open the file to be able to read from it
+	FILE* file;
+	fopen_s(&file, filePath, "r"); // Open the file to be able to read from it
 	if (file == NULL) {
 		MessageBox(NULL, L"Could not open file while loading OBJ", NULL, MB_ICONEXCLAMATION);
 		return false;
@@ -28,29 +36,29 @@ bool loadOBJ(const char* filePath, std::vector <Vertex> &vertices) {
 	bool readingFile = true;
 	while (readingFile) {
 		char lineHeader[128];
-		int result = fscanf(file, "%s", lineHeader);
+		int result = fscanf_s(file, "%s", lineHeader);
 		if (result == EOF) { // Terminate if end of file has been reached
 			readingFile = false;
 		}
 		else {
 			if (strcmp(lineHeader, "v") == 0) { // Current line to read is vertex info
 				XMFLOAT3 vertex;
-				fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+				fscanf_s(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
 				tempVerts.push_back(vertex);
 			}
 			else if (strcmp(lineHeader, "vt") == 0) { // Current line to read is texcoord info
 				XMFLOAT2 texCoord;
-				fscanf(file, "%f %f\n", &texCoord.x, &texCoord.y);
+				fscanf_s(file, "%f %f\n", &texCoord.x, &texCoord.y);
 				tempTexCoords.push_back(texCoord);
 			}
 			else if (strcmp(lineHeader, "vn") == 0) { // Current line to read is normal info
 				XMFLOAT3 normal;
-				fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+				fscanf_s(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
 				tempNormals.push_back(normal);
 			}
 			else if (strcmp(lineHeader, "f") == 0) { // Current line to read is face info
 				unsigned int vertexIndex[3], texCoordIndex[3], normalIndex[3];
-				int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &texCoordIndex[0], &normalIndex[0], 
+				int matches = fscanf_s(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &texCoordIndex[0], &normalIndex[0], 
 					&vertexIndex[1], &texCoordIndex[1], &normalIndex[1], &vertexIndex[2], &texCoordIndex[2], &normalIndex[2]);
 				if (matches != 9) {
 					MessageBox(NULL, L"Face data in OBJ file does not match format of the OBJ reader!", NULL, MB_ICONEXCLAMATION);
